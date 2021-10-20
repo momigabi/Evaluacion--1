56927ac3c9b4c8b7eaebe412bafbf23b429c2012
momigabi <92833380+momigabi@users.noreply.github.com>
2021-10-19 21:04:22 -0400

    Evaluación 1 de programación
    Primera evaluación de programación del primer corte elaborado por Monica Bernal

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+namespace Primer_trabajo
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+         
+		
+		Console.WriteLine("TRES DIGITOS");
+		Console.WriteLine(" ");
+		
+		int a = 3 ; 
+		int b = 8 ; 
+		int c = 1 ;
+			
+		Console.WriteLine("a="+a);
+		Console.WriteLine("b="+b); 
+		Console.WriteLine("c="+c);
+		 
+		Console.WriteLine(" ");
+
+		Console.WriteLine("aacb");
+		Console.Write(a);
+		Console.Write(a);
+		Console.Write(c);
+		Console.WriteLine(b);
+		
+		Console.WriteLine(" ");
+		Console.WriteLine("bca");
+		Console.Write(b);
+		Console.Write(c);
+		Console.Write(a);
+		
+		Console.WriteLine("  ");
+		Console.WriteLine("");
+		
+		Console.WriteLine("bcbc");
+		Console.Write(b);
+		Console.Write(c);
+		Console.Write(b);
+		Console.Write(c);
+	
+        }
+		
+	}
+}
+		
+         
+         
+        
+         
+            
